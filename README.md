238d8a1b86ed9ab5e1bb81063ac14ac3e808fbd7
Alex <alterrible07@gmail.com>
2023-12-05 08:52:18 +0100

    V.2
    - cour NSI

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 V.1
 
 - cristmas update de maths
+
+V.2
+
+- cour NSI
--- sapin.py
+++ sapin.py
@@ -1,17 +1,25 @@
 # cristmas update de maths.py
-sage=str(input("avez vous été sage cette année ?  ")) # Christmas update
-sapin = [
-    [None, None, None, "*", None, None, None],
-    [None, None, None, "-", None, None, None],
-    [None, None, "-", "-", "-", None, None],
-    [None, "-", "-", "-", "-", "-", None],
-    ["-", "-", "-", "-", "-", "-", "-"],
-    [None, None, None, "-", None, None, None]
-]
+# sage=str(input("avez vous été sage cette année ?  ")) # Christmas update
+# sapin = [
+#     [None, None, None, "*", None, None, None],
+#     [None, None, None, "-", None, None, None],
+#     [None, None, "-", "-", "-", None, None],
+#     [None, "-", "-", "-", "-", "-", None],
+#     ["-", "-", "-", "-", "-", "-", "-"],
+#     [None, None, None, "-", None, None, None]
+# ]
 
-if sage=="oui" or "OUI" or "Oui":
-    for row in sapin:
-        print(' '.join(str(e) if e else ' ' for e in row))
-    print("joyeux noël")
-else:
-    print("RIP")
+# if sage=="oui" or "OUI" or "Oui":
+#     for row in sapin:
+#         print(' '.join(str(e) if e else ' ' for e in row))
+#     print("joyeux noël")
+# else:
+#     print("RIP")
+
+a=int(input("sapin (nb impaire): "))
+def sapin(largeur): #la largeur est impaire
+    dl=largeur//2
+    for i in range(dl+1):
+        ligne=" "*(dl-i)+"^"*i+"^"+"^"*i
+        print(ligne)
+sapin(a)
